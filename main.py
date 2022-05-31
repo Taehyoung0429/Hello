73f0d9a8df6d9a9727b731f805abf288656622d6
Taehyoung0429 <93691713+Taehyoung0429@users.noreply.github.com>
2022-05-31 15:11:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,14 @@ matic_long = 100957.149571
 matic_rent = 102043.221440
 previous_price = (usdc_long*matic_long)/((matic_rent-binance_matic)**2)
 volatility_range = 2.5
-text = print(previous_price)
+print(previous_price)
+text = previous_price
 bot.sendMessage(chat_id = 1489495601, text=text)
 
 matic = binance.fetch_ticker("MATIC/USDT")
 matic_price = matic['last']
-text = print(matic_price)
+print(matic_price)
+text = matic_price
 bot.sendMessage(chat_id = 1489495601, text=text)
 
 if abs(((matic_price - previous_price)/previous_price)*100) > volatility_range:
@@ -41,12 +43,14 @@ if abs(((matic_price - previous_price)/previous_price)*100) > volatility_range:
 
   if required_change > 0:
     order = binance.create_market_buy_order(symbol="MATIC/USDT", amount= required_change)
-    text = print('order = binance.create_market_buy_order(symbol="MATIC/USDT", amount= required_change)')
+    print('order = binance.create_market_buy_order(symbol="MATIC/USDT", amount= required_change)')
+    text = order = binance.create_market_buy_order(symbol="MATIC/USDT", amount= required_change)
     bot.sendMessage(chat_id = 1489495601, text=text)
   else:
     required_change = required_change*-1 
     order = binance.create_market_sell_order(symbol="MATIC/USDT", amount= required_change)
-    text = print('order = binance.create_market_sell_order(symbol="MATIC/USDT", amount= required_change)')
+    print('order = binance.create_market_sell_order(symbol="MATIC/USDT", amount= required_change)')
+    text = order = binance.create_market_sell_order(symbol="MATIC/USDT", amount= required_change)
     bot.sendMessage(chat_id = 1489495601, text=text)
 else:
   pass
